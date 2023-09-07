e54d76d3c61f1556ce3ee5f8496cd9023d97a8a8
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-09-07 22:06:43 +0200

    clean some code related to tests on windows

--- test/extended-contract-test-helpers.ts
+++ test/extended-contract-test-helpers.ts
@@ -9,13 +9,7 @@ import {
 import { ExtendedContract } from '../src';
 import { Web3BaseProvider } from 'web3';
 
-console.log('process.env.RUNNER_OS', process.env.RUNNER_OS);
-console.log('process.env.MATRIX_OS', process.env.MATRIX_OS);
-export const itSkipIfWindows =
-  process.env.RUNNER_OS !== 'Windows' &&
-  process.env.MATRIX_OS !== 'windows-latest'
-    ? it
-    : it.skip;
+export const itSkipIfWindows = process.env.RUNNER_OS !== 'Windows' ? it : it.skip;
 
 export async function testSuccessfulCompilation(
   ExtendedContractType: typeof ExtendedContract
