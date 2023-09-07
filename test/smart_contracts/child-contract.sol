a3ac5eeddfada90610cd9ed0b1fd0f5d03bdf333
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-09-07 17:34:34 +0200

    try fixing solidity imports for windows

--- test/smart_contracts/child-contract.sol
+++ test/smart_contracts/child-contract.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// The following works on linux, macOS and windows
+// The following works windows
 // However, if you need to run only on linux or macOS you can use: import "./simple-contract.sol";
-import "simple-contract.sol"; 
+import ".\simple-contract.sol"; 
 contract ChildContract is SimpleContract {
     string public myText;
 
